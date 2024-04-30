e0df900a2a9465b04208d4317c59b0487ab2feab
frustrated018 <niloyd018@gmail.com>
2024-05-01 00:01:40 +0600

    Finally done with the basic report card

--- src/components/custom/Report.tsx
+++ src/components/custom/Report.tsx
@@ -1,8 +1,12 @@
-import { Filter } from "lucide-react";
+"use client";
+import { Cog, Filter } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
+import { DatePickerWithRange } from "./DatePickerWithRange";
+import { Checkbox } from "../ui/checkbox";
+import { toast } from "sonner";
 
 const Report = () => {
   return (
@@ -19,26 +23,54 @@ const Report = () => {
                 Filters <Filter />
               </CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-6 gap-5">
+            <CardContent className="grid grid-cols-7 gap-5">
+              <div className="col-span-2">
+                <Label>Date Range:</Label>
+                <DatePickerWithRange />
+              </div>
               <div>
-                <Label>Blah</Label>
+                <Label>Accountant</Label>
                 <Input />
               </div>
               <div>
-                <Label>Blah</Label>
+                <Label>Audit</Label>
                 <Input />
               </div>
               <div>
-                <Label>Blah</Label>
+                <Label>Type</Label>
                 <Input />
               </div>
+              <section className="space-y-2">
+                <Label>Show Details</Label>
+                <div className="space-y-3">
+                  <div className="flex items-center space-x-2">
+                    <Checkbox />
+                    <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Show group Tests
+                    </Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Checkbox />
+                    <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Show Expenses
+                    </Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Checkbox />
+                    <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Show profits
+                    </Label>
+                  </div>
+                </div>
+              </section>
               <div>
-                <Label>Blah</Label>
-                <Input />
-              </div>
-              <div>Bunch of checkbox</div>
-              <div>
-                <Button>Bruhhh</Button>
+                <Button
+                  onClick={() => {
+                    toast.info("This function is not available in demo.");
+                  }}
+                >
+                  <Cog />
+                </Button>
               </div>
             </CardContent>
           </Card>
