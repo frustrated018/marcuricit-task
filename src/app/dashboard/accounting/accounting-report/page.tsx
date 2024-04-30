a7f31e3c80c9c7dc66ae72c6fea8f152ac1ab21b
frustrated018 <niloyd018@gmail.com>
2024-05-01 00:44:43 +0600

    Added beadcrumbs

--- src/app/dashboard/accounting/accounting-report/page.tsx
+++ src/app/dashboard/accounting/accounting-report/page.tsx
@@ -4,10 +4,10 @@ const AccountingReportePage = () => {
   return (
     <div className="bg-secondary min-h-screen p-5">
       <Report
+        reportName="Accounting"
         input1="Accountant"
         input2="Audit"
         input3="Type"
-        reportName="Accounting"
       />
     </div>
   );
--- src/app/dashboard/accounting/doctor-report/page.tsx
+++ src/app/dashboard/accounting/doctor-report/page.tsx
@@ -4,10 +4,10 @@ const DoctorReportPage = () => {
   return (
     <div className="bg-secondary min-h-screen p-5">
       <Report
+        reportName="Doctor"
         input1="Doctors"
         input2="Type"
         input3="Culture"
-        reportName="Doctor"
       />
     </div>
   );
