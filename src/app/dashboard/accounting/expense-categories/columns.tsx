6665f80b91ce8b2d5219c84e7547cd7b023d17c3
frustrated018 <niloyd018@gmail.com>
2024-04-30 23:18:00 +0600

    Expenses categories page Finished

---
+++ src/app/dashboard/accounting/expense-categories/columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { Pencil, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+
+export type ExpenseCategories = {
+  id: string;
+  category: string;
+  date: string;
+  amount: number;
+};
+
+export const columns: ColumnDef<ExpenseCategories>[] = [
+  {
+    accessorKey: "id",
+    header: "#",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    header: "Actions",
+    cell: () => {
+      return (
+        <div>
+          <Button
+            size="sm"
+            variant="ghost"
+            className="hover:text-emerald-500"
+            onClick={() => {
+              toast.info("Edit function is not available in demo.");
+            }}
+          >
+            <Pencil />
+          </Button>
+          <Button
+            size="sm"
+            variant="ghost"
+            className="hover:text-red-500"
+            onClick={() => {
+              toast.info("Delete function is not available in demo.");
+            }}
+          >
+            <Trash2 />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
--- src/app/dashboard/accounting/expense-categories/page.tsx
+++ src/app/dashboard/accounting/expense-categories/page.tsx
@@ -1,10 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-react";
+import { ExpenseCategories, columns } from "./columns";
+import expenseData from "@/assets/expenseData.json";
 
-const ExpenseCategories = () => {
-  return (
-    <div>
-        This will be the expense categories page
-    </div>
-  )
+// TODO: Add Breadcrumbs
+
+async function getData(): Promise<ExpenseCategories[]> {
+  // This is where I would fetch data
+  return expenseData as ExpenseCategories[];
 }
 
-export default ExpenseCategories
+const ExpenseCategoriesPage = async () => {
+  const data = await getData();
+
+  return (
+    <section className="bg-secondary min-h-screen py-5">
+      <div className="flex justify-between items-center container">
+        <Button>
+          Add record
+          <Plus />
+        </Button>
+        <Input
+          type="text"
+          placeholder="Search..."
+          className=" bg-background max-w-[180px]"
+        />
+      </div>
+      <div className="container mx-auto mt-5">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </section>
+  );
+};
+
+export default ExpenseCategoriesPage;
